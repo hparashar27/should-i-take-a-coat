8ad7e69b2683edb3ce8c5187972dfe31b4b97112
Joshua Hargreaves <me@joshhargreaves.co.uk>
2023-01-13 22:47:47 +0000

    [FEATURE] - Add initial Design

---
+++ components/layout/layout.js
@@ -0,0 +1,9 @@
+export default function Layout({ children }) {
+    return (
+        <div className="h-screen lg:flex flex-wrap justify-between">
+            <main className="flex-1">
+                {children}
+            </main>
+        </div>
+    );
+}
