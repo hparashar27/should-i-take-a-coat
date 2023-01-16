43e421cf3c0c2a5a0ab102a5d9fb3596308a6519
Harsh <hparashar617@gmail.com>
2023-01-16 12:15:28 +0530

    implementation of the header

---
+++ components/layout/header.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import {TiWeatherPartlySunny} from "react-icons/ti"
+import {TiWeatherSnow} from "react-icons/ti"
+const Header = () => {
+  return (
+    <header className="bg-gray-900 text-white p-6">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link href="/"
+          className=" text-xl "><TiWeatherPartlySunny />
+        </Link>
+        <nav className="flex justify-center">
+          <ul className="flex">
+            <li className="ml-6">
+              <Link href="/"
+                className="text-gray-500 hover:text-white">Home
+              </Link>
+            </li>
+            <li className="ml-6">
+              <Link href="/about"
+               className="text-gray-500 hover:text-white">About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <i >
+            <TiWeatherSnow/>
+        </i>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,7 +1,8 @@
 import Footer from './footer'
-
+import  Header  from './header'
 export default function Layout({ children }) {
     return (<>
+        <Header/>
         <div className="h-screen lg:flex flex-wrap justify-between">
             <main className="flex-1">
                {children}
